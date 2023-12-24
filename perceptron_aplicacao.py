7235cebeb0b4ffef3b7087e247d19344528f556d
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-12-24 14:13:00 -0300

    finalização de perceptron 1 camada

--- perceptron_aplicacao.py
+++ perceptron_aplicacao.py
@@ -4,7 +4,8 @@ import numpy as np
 # saidas = np.array([0,0,0,1])
 # pesos = np.array([0.0,0.0])
 entradas = np.array([[0,0, 0],[0,0,1],[0, 1,0],[0,1, 1],[1,0, 0],[1,0,1],[1, 1,0],[1,1, 1]])
-saidas = np.array([0,0,0,0,0,0,0,1])
+# saidas = np.array([0,0,0,0,0,0,0,1]) #AND
+saidas = np.array([0,1,1,1,1,1,1,1])
 pesos = np.array([0.0,0.0,0.0])
 taxa_aprendizagem = 0.1
 
@@ -24,16 +25,30 @@ def calcula_saida(registro):
 
 def treinar():
     erro_total = 1
+    iteracao = 1
     while(erro_total != 0):
+        print(f'\nItereção:  {iteracao}')
         erro_total = 0
         for i in range(len(saidas)):
+            print(f'Entrada {i} {np.asarray(entradas[i])}')
             saida_calculada = calcula_saida(np.asarray(entradas[i]))
-            erro = saidas[i] - saida_calculada
+            print(f'Saída calculada - Entrada {i} - {saida_calculada}')
+            erro = abs(saidas[i] - saida_calculada)
             erro_total += erro
+            
             for j in range(len(pesos)):
                 pesos[j] = pesos[j] + (taxa_aprendizagem * entradas[i][j] * erro)  
-                print('Peso atualizado: ' + str(pesos[j]))
+                print(f'Peso atualizado {entradas[i][j]}: {str(pesos[j])}')
         
         print('Erro total:', str(erro_total))
+        iteracao+=1
 
 treinar()
+
+print('Rede neural treinada')
+print(pesos)
+
+print(calcula_saida(entradas[0]))
+print(calcula_saida(entradas[1]))
+print(calcula_saida(entradas[2]))
+print(calcula_saida(entradas[7]))
